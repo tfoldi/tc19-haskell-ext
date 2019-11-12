bccded5e9df567e0c7a52ea435f5517f3d94a5fc
Földi Tamás <tfoldi@xsi.hu>
2019-11-12 09:44:12 -0800

    Servering /info with constant json payload

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+
 module Main where
 
-import Lib
+import Data.Aeson
+import Data.Text
+import qualified Data.Text.Lazy as T
+import GHC.Generics
+import Data.HashMap.Strict 
+import qualified Data.Text.Lazy.Encoding as T
+import Web.Scotty
+import System.Process
+import Control.Monad.IO.Class (liftIO)
 
-main :: IO ()
-main = someFunc
+data EvalRequest = 
+    EvalRequest { 
+        _script :: Text, 
+        _data :: HashMap Text Value
+      } deriving (Show, Generic, ToJSON)
+
+instance FromJSON EvalRequest 
+    where parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = Prelude.drop 1 }
+
+
+infoResponse = "{\"description\": \"\", \"creation_time\": \"0\", \"state_path\": \"\", \"server_version\": \"0.8.7\", \"name\": \"haskell\", \"versions\": {\"v1\": {\"features\": {}}}}"
+
+textAsJson t = setHeader "Content-Type" "application/json" >> text t
+
+main = scotty 3000 $ do
+    get "/info" $ 
+        textAsJson infoResponse
+    post "/evaluate" $ do
+        jsonData <- body 
+        cmdRes <- liftIO $ executeCommand jsonData
+        textAsJson $ T.pack cmdRes
